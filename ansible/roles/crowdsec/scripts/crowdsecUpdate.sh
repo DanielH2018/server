2625a7c1db48f4a5c822fe8568f4e850cc0e66f5
DanielH2018 <danielh.2018@gmail.com>
2022-02-24 18:51:28 -0500

    Add var for string.

--- ansible/roles/crowdsec/scripts/crowdsecUpdate.sh
+++ ansible/roles/crowdsec/scripts/crowdsecUpdate.sh
@@ -1,9 +1,12 @@
 # Copy current crontab
 crontab -l > crontab_new
 
+# Declare Var
+CRONJOB="15 7 * * * docker exec crowdsec cscli hub update && docker exec crowdsec cscli hub upgrade"
+
 # Add update and upgrade crontab at 7:15am UTC(2:15am EST)
-if grep -q "15 7 * * * docker exec crowdsec cscli hub update && docker exec crowdsec cscli hub upgrade" crontab_new; then
-echo "15 7 * * * docker exec crowdsec cscli hub update && docker exec crowdsec cscli hub upgrade" >> crontab_new
+if grep -q "$CRONJOB" crontab_new; then
+echo "$CRONJOB" >> crontab_new
 fi
 
 # Commit and Cleanup
