95aa856047907ad375f3b5a40ef0498c52a06063
DanielH2018 <danielh.2018@gmail.com>
2022-02-24 18:44:30 -0500

    Update script.

--- ansible/roles/crowdsec/scripts/crowdsecUpdate.sh
+++ ansible/roles/crowdsec/scripts/crowdsecUpdate.sh
@@ -2,7 +2,9 @@
 crontab -l > crontab_new
 
 # Add update and upgrade crontab at 7:15am UTC(2:15am EST)
+if grep -q "15 7 * * * docker exec crowdsec cscli hub update && docker exec crowdsec cscli hub upgrade" crontab_new; then
 echo "15 7 * * * docker exec crowdsec cscli hub update && docker exec crowdsec cscli hub upgrade" >> crontab_new
+fi
 
 # Commit and Cleanup
 crontab crontab_new
