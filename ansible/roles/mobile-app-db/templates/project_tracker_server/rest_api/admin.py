dcf84083e91696fd9f5c9ce0a4143b6b619c1a8b
DanielH2018 <danielh.2018@gmail.com>
2022-02-26 16:29:26 -0500

    Update method.

--- ansible/roles/mobile-app-db/templates/project_tracker_server/rest_api/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from rest_api.models import Project, ProjectMembership, Task
-
-admin.site.register(Project)
-admin.site.register(ProjectMembership)
-admin.site.register(Task)
