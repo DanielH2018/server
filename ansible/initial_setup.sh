ffda965743c4735d982d0c98e66ff4c0f55ea363
DanielH2018 <danielh.2018@gmail.com>
2022-09-06 13:51:46 +0000

    Move docker image prune to non-sudo.

--- ansible/initial_setup.sh
+++ ansible/initial_setup.sh
@@ -14,17 +14,24 @@ sudo sed -i '/PermitRootLogin /c\PermitRootLogin no' /etc/ssh/sshd_config
 sudo sed -i '/IgnoreRhosts /c\IgnoreRhosts yes' /etc/ssh/sshd_config
 sudo sed -i '/ChallengeResponseAuthentication  /c\ChallengeResponseAuthentication no' /etc/ssh/sshd_config
 
-# Copy current crontab
+# Copy current sudo crontab
 sudo crontab -l > crontab_new
-
-# Clean dangling Docker images
-echo "30 6 * * * /usr/bin/docker image prune -a -f" >> crontab_new
 
 # Schedule restart at 7:30am UTC(2:30am EST) on Sunday
 echo "30 7 * * 0 /sbin/shutdown -r +5" >> crontab_new
 
 # Commit and Cleanup
 sudo crontab crontab_new
+rm crontab_new
+
+# Copy current crontab
+crontab -l > crontab_new
+
+# Clean dangling Docker images
+echo "30 6 * * * /usr/bin/docker image prune -a -f" >> crontab_new
+
+# Commit and Cleanup
+crontab crontab_new
 rm crontab_new
 
 # Allow SSH through firewall
