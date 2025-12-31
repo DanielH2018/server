58110c9e593a526f9a9f8d3327bed888c1ab775d
DanielH2018 <danielh.2018@gmail.com>
2025-12-31 20:16:32 +0000

    Update Setup Script.

--- ansible/playbooks/setup/pi/initial_setup.sh
+++ ansible/playbooks/setup/pi/initial_setup.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+# Set Playbook Directory
+ANSIBLE_PLAYBOOK_DIR="/home/ubuntu/server/ansible/playbooks/"
 # Install ansible
 sudo apt install ansible -y
 # Run initial setup playbook
-ansible-playbook /home/ubuntu/server/ansible/initial_setup.yml -K
+ansible-playbook ${ANSIBLE_PLAYBOOK_DIR}/setup/pi/initial_setup.yml -K
+# Run Docker Setup playbook
+ansible-playbook ${ANSIBLE_PLAYBOOK_DIR}/setup-docker.yml -K
 # Run Raspberry Pi optimization playbook
-ansible-playbook /home/ubuntu/server/ansible/setup/pi/optimize_pi.yml -K
+ansible-playbook ${ANSIBLE_PLAYBOOK_DIR}/setup/pi/optimize_pi.yml -K
