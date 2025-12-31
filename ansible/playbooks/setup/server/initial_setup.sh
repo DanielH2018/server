d32860b43bb761b005c0cdc43cf027f7498aaa8c
DanielH2018 <danielh.2018@gmail.com>
2025-12-31 20:49:04 +0000

    Add config-files playbook.

--- ansible/playbooks/setup/server/initial_setup.sh
+++ ansible/playbooks/setup/server/initial_setup.sh
@@ -5,6 +5,8 @@ ANSIBLE_PLAYBOOK_DIR="/home/ubuntu/server/ansible/playbooks/"
 
 # Install ansible
 sudo apt install ansible -y
+# Run Config Files playbook
+ansible-playbook ${ANSIBLE_PLAYBOOK_DIR}/setup/config_files.yml -K
 # Run initial setup playbook
 ansible-playbook ${ANSIBLE_PLAYBOOK_DIR}/setup/server/initial_setup.yml -K
 # Run Docker Setup playbook
